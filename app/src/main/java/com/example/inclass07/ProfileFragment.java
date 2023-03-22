cd54998dcf0a56b61178d5d3a40ed516492bd9eb
bharath sanjay <bharathsanjay4321@gmail.com>
2023-03-21 23:52:44 -0400

    profilee

--- app/src/main/java/com/example/inclass07/ProfileFragment.java
+++ app/src/main/java/com/example/inclass07/ProfileFragment.java
@@ -1,14 +1,34 @@
 package com.example.inclass07;
 
+import android.content.Context;
+import android.content.SharedPreferences;
 import android.os.Bundle;
 
+import androidx.annotation.NonNull;
 import androidx.fragment.app.Fragment;
 
+import android.os.Handler;
+import android.os.Looper;
+import android.util.Log;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 import android.widget.Button;
 import android.widget.TextView;
+import android.widget.Toast;
+
+import com.google.gson.Gson;
+
+import java.io.IOException;
+
+import okhttp3.Call;
+import okhttp3.Callback;
+import okhttp3.FormBody;
+import okhttp3.OkHttpClient;
+import okhttp3.Request;
+import okhttp3.RequestBody;
+import okhttp3.Response;
+import okhttp3.ResponseBody;
 
 /**
  * A simple {@link Fragment} subclass.
@@ -25,8 +45,12 @@ public class ProfileFragment extends Fragment {
     // TODO: Rename and change types of parameters
     private String mParam1;
     private String mParam2;
+    private OkHttpClient client = new OkHttpClient();
 
     private Button back_btn;
+    private String BASE_URL = "http://ec2-54-164-201-39.compute-1.amazonaws.com:3000/api/auth/me";
+
+    private Handler handler = new Handler(Looper.getMainLooper());
 
     private TextView name;
 
@@ -63,15 +87,105 @@ public class ProfileFragment extends Fragment {
         }
     }
 
+    private class Me{
+        private String _id;
+        private String name;
+        private String email;
+
+
+        public String get_id() {
+            return _id;
+        }
+
+        public String getName() {
+            return name;
+        }
+
+        public String getEmail() {
+            return email;
+        }
+    }
+
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
+
         // Inflate the layout for this fragment
         View view = inflater.inflate(R.layout.fragment_profile, container, false);
 
         back_btn = view.findViewById(R.id.disp_back);
         name = view.findViewById(R.id.disp_name);
         email = view.findViewById(R.id.disp_email);
+
+
+        SharedPreferences sharedPref = getActivity().getSharedPreferences(
+                "data", Context.MODE_PRIVATE);
+        String logintoken = sharedPref.getString(getString(R.string.login_data), "empty");
+
+
+        Request request = new Request.Builder()
+                .url(BASE_URL)
+                .addHeader("x-access-token", logintoken)
+                .build();
+
+        client.newCall(request).enqueue(new Callback() {
+            @Override
+            public void onFailure(@NonNull Call call, @NonNull IOException e) {
+                e.printStackTrace();
+                Log.d("fail", "onFailure: " + "failiure");
+
+
+            }
+
+            @Override
+            public void onResponse(@NonNull Call call, @NonNull Response response) throws IOException {
+                if (response.isSuccessful()) {
+                    ResponseBody responseBody = response.body();
+                    //Log.d("onresponse", "onResponse: " + responseBody.string());
+                    System.out.println("success");
+
+
+                    // gson parsing
+                    Gson gson = new Gson();
+                    Me me = gson.fromJson(responseBody.string(), Me.class);
+
+                    handler.post(new Runnable() {
+                        @Override
+                        public void run() {
+
+                            name.setText(me.getName());
+                            email.setText(me.getEmail());
+
+
+
+                        }});
+
+
+
+                    Log.d("login data", "onResponse: " + R.string.login_data);
+                } else {
+                    Log.d("not success", "onResponse: " + response.body());
+                    handler.post(new Runnable() {
+                        @Override
+                        public void run() {
+                            Toast.makeText(getContext(), "invalid credentials",Toast.LENGTH_SHORT).show();
+
+                        }});
+
+                }
+
+
+            }
+
+        });
+
+        back_btn.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                getActivity().getSupportFragmentManager().beginTransaction().replace(R.id.rootlayout, new NotesDisplay(), "newnotetodisplaynote").commit();
+            }
+        });
+
 
 
         return view;
